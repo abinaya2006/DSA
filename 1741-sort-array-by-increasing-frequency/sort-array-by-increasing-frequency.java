aeea5b9dad172259838f7152db43b740099fbfb6
Abinaya .☘︎ ݁˖ <72507845+abinaya2006@users.noreply.github.com>
2026-09-08 15:39:33 +0530

    Time: 10 ms (20.12%) | Memory: 45.7 MB (48.64%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -6,18 +6,35 @@ class Solution {
             h.put(x,h.getOrDefault(x,0)+1);
         }
 
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++)
-            {
-                if(h.get(nums[j])<h.get(nums[i]) || h.get(nums[j]).equals(h.get(nums[i])) && nums[j]>nums[i]){
-                    int temp=nums[i];
-                    nums[i]=nums[j];
-                    nums[j]=temp;
-                }
-                
-            }
+        List<Integer> list=new ArrayList<>();
+
+        for(int x:nums){
+            list.add(x);
         }
 
+        // for(int i=0;i<nums.length-1;i++){
+        //     for(int j=i+1;j<nums.length;j++)
+        //     {
+        //         if(h.get(nums[j])<h.get(nums[i]) || h.get(nums[j]).equals(h.get(nums[i])) && nums[j]>nums[i]){
+        //             int temp=nums[i];
+        //             nums[i]=nums[j];
+        //             nums[j]=temp;
+        //         }
+                
+        //     }
+        // }
+
+        Collections.sort(list,(a,b)->{
+            if(!h.get(a).equals(h.get(b))){
+                return h.get(a)-h.get(b);
+            }
+            return b-a;
+        });
+
+        for(int i=0;i<list.size();i++){
+            nums[i]=list.get(i);
+        }
+        
         return nums;
     }
 }
